ca649ac3c754e43a26875618007f131e90a4602a
prashantn <prashant.nandipati@zigram.tech>
2024-11-21 15:21:27 +0000

    simplify

--- cliq/main.go
+++ cliq/main.go
@@ -16,9 +16,9 @@ func (c CLIQ_CONFIG) CliqEndpoint() string {
 	return fmt.Sprintf("%s?zapikey=%s", c.WebhookEndpoint, c.ZAPIKey)
 }
 
-func (c CLIQ_CONFIG) SendMessageToCliq(heading string, message string) error {
+func (c CLIQ_CONFIG) SendMessageToCliq(message string) error {
 	payload, _ := json.Marshal(map[string]string{
-		"text": "*" + heading + "*\n\n" + message,
+		"text": message,
 	})
 	resp, err := http.Post(c.CliqEndpoint(), "application/json", bytes.NewBuffer(payload))
 	if err != nil {
